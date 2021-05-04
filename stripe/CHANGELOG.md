c2abf248f3a3de389a0d59affc0042120c2778b9
Jonas Bark <jonas.bark@gmx.de>
2021-05-04 17:05:12 +0200

    add more information to the README.md, export stripe platform interface

--- stripe/CHANGELOG.md
+++ stripe/CHANGELOG.md
@@ -1,3 +1,3 @@
-## 0.0.1
+## 1.0.0+3
 
-* TODO: Describe initial release.
+* Initial beta release
--- stripe/lib/flutter_stripe.dart
+++ stripe/lib/flutter_stripe.dart
@@ -1,3 +1,5 @@
+export 'package:stripe_platform_interface/stripe_platform_interface.dart';
+
 export 'src/stripe.dart';
 export 'src/widgets/apple_pay_button.dart';
 export 'src/widgets/card_field.dart';
