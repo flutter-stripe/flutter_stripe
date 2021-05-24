716cf5b8f08bfb3f1fb386edb7bd8b366ede1e2f
Jonas Bark <jonas.bark@gmx.de>
2021-05-24 16:40:46 +0200

    fix env-files.sh

--- .github/workflows/scripts/env-files.sh
+++ .github/workflows/scripts/env-files.sh
@@ -17,12 +17,12 @@ cp .env.example .env
 
 # The syntax of -i is one difference between GNU sed and sed from mac os
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  sed -i '' "s/<ADD_YOUR_KEY_HERE>/$STRIPE_PUBLISHABLE_KEY/" ./.env.dart
+  sed -i '' "s/<ADD_YOUR_KEY_HERE>/$STRIPE_PUBLISHABLE_KEY/" ./.env
 else
-  sed -i "s/<ADD_YOUR_KEY_HERE>/$STRIPE_PUBLISHABLE_KEY/" ./.env.dart
+  sed -i "s/<ADD_YOUR_KEY_HERE>/$STRIPE_PUBLISHABLE_KEY/" ./.env
 fi
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  sed -i '' "s/<ADD_YOUR_SECRET_KEY_HERE>/$STRIPE_SECRET_KEY/" ./.env.dart
+  sed -i '' "s/<ADD_YOUR_SECRET_KEY_HERE>/$STRIPE_SECRET_KEY/" ./.env
 else
-  sed -i "s/<ADD_YOUR_SECRET_KEY_HERE>/$STRIPE_SECRET_KEY/" ./.env.dart
+  sed -i "s/<ADD_YOUR_SECRET_KEY_HERE>/$STRIPE_SECRET_KEY/" ./.env
 fi
