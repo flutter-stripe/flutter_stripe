3897a4c3470a1db524ad3a160a575a002391e158
Remon <remon@hey.com>
2021-05-29 14:49:57 +0200

    document errors and clean up not needed enums

--- packages/stripe_platform_interface/lib/src/models/errors.dart
+++ packages/stripe_platform_interface/lib/src/models/errors.dart
@@ -2,20 +2,13 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'errors.freezed.dart';
 part 'errors.g.dart';
+
 // ignore_for_file: constant_identifier_names
-
-enum ConfirmPaymentError { canceled, failed, unknown }
-
-enum CardActionError { canceled, failed, unknown }
-
-enum ConfirmSetupIntentError { canceled, failed, unknown }
-
-enum CreatePaymentMethodError { failed }
 enum PaymentIntentError { unknown }
 
-enum ApplePayError { canceled, failed, unknown }
-
 @freezed
+
+/// Wrapper class that represents an error with the Stripe platform.
 class StripeError<T> with _$StripeError<T>, Exception {
   @JsonSerializable(explicitToJson: true)
   const factory StripeError({
@@ -31,14 +24,3 @@ class StripeError<T> with _$StripeError<T>, Exception {
 T _dataFromJson<T>(Map<String, dynamic> input) => input['code'] as T;
 
 Map<String, dynamic> _dataToJson<T>(T input) => {'code': input};
-
-enum LastPaymentErrorType {
-  ApiConnection,
-  Api,
-  Authentication,
-  Card,
-  Idempotency,
-  InvalidRequest,
-  RateLimit,
-  Unknown
-}
