5dea8e79a6906cfe0a5f9fc19af32568395bb73c
Jaime Blasco <git@jaimeblasco.com>
2021-08-30 19:44:48 +0200

    Update merchant identifier

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -8,7 +8,7 @@ import 'widgets/dismiss_focus_overlay.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Stripe.publishableKey = stripePublishableKey;
-  Stripe.merchantIdentifier = 'merchant.com.stripe.test';
+  Stripe.merchantIdentifier = 'merchant.flutter.stripe.test';
   Stripe.urlScheme = 'flutterstripe';
   await Stripe.instance.applySettings();
   runApp(const App());
