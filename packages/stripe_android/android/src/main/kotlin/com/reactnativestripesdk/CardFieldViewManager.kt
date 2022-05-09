1a23ed6eb4ef11da070b3b186f6ab99f244e4503
Remon <remon@hey.com>
2022-05-09 19:40:50 +0200

    fix compile issues android

--- packages/stripe_android/android/src/main/kotlin/com/reactnativestripesdk/CardFieldViewManager.kt
+++ packages/stripe_android/android/src/main/kotlin/com/reactnativestripesdk/CardFieldViewManager.kt
@@ -6,6 +6,7 @@ import com.facebook.react.common.MapBuilder
 import com.facebook.react.uimanager.SimpleViewManager
 import com.facebook.react.uimanager.ThemedReactContext
 import com.facebook.react.uimanager.annotations.ReactProp
+import com.stripe.android.model.PaymentMethodCreateParams
 
 class CardFieldViewManager : SimpleViewManager<CardFieldView>() {
   override fun getName() = "CardField"
