b9f974ee94eaa26bd539fd63f1712bc8ff948303
Remon <remonhelmond@gmail.com>
2023-01-12 21:01:30 +0000

    create sofort payment example

---
+++ example/lib/screens/regional_payment_methods/sofort_screen.dart
@@ -0,0 +1,89 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:http/http.dart' as http;
+import 'package:stripe_example/widgets/example_scaffold.dart';
+import 'package:stripe_example/widgets/loading_button.dart';
+
+import '../../config.dart';
+
+class SofortScreen extends StatelessWidget {
+  const SofortScreen({Key? key}) : super(key: key);
+
+  Future<Map<String, dynamic>> _createPaymentIntent() async {
+    final url = Uri.parse('$kApiUrl/create-payment-intent');
+    final response = await http.post(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: json.encode({
+        'currency': 'eur',
+        'payment_method_types': ['sofort'],
+        'amount': 1099
+      }),
+    );
+
+    return json.decode(response.body);
+  }
+
+  Future<void> _pay(BuildContext context) async {
+    // Precondition:
+    //Make sure to have set a custom URI scheme in your app and add it to Stripe SDK
+    // see file main.dart in this example app.
+    // 1. on the backend create a payment intent for payment method and save the
+    // client secret.
+    final result = await _createPaymentIntent();
+    final clientSecret = await result['clientSecret'];
+
+    // 2. use the client secret to confirm the payment and handle the result.
+    try {
+      await Stripe.instance.confirmPayment(
+        paymentIntentClientSecret: clientSecret,
+        data: PaymentMethodParams.sofort(
+          paymentMethodData: PaymentMethodDataSofort(country: 'de'),
+        ),
+      );
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Payment succesfully completed'),
+        ),
+      );
+    } on Exception catch (e) {
+      if (e is StripeException) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(
+                'Error from Stripe: ${e.error.localizedMessage ?? e.error.code}'),
+          ),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Unforeseen error: ${e}'),
+          ),
+        );
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ExampleScaffold(
+      title: 'Sofort',
+      tags: ['Payment method'],
+      padding: EdgeInsets.all(16),
+      children: [
+        LoadingButton(
+          onPressed: () async {
+            await _pay(context);
+          },
+          text: 'Pay',
+        ),
+      ],
+    );
+  }
+}
