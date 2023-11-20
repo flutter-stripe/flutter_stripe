bf1fdf4365c76c58559c906e1c8414ad2303339e
Remon <remonhelmond+dev@gmail.com>
2023-11-20 20:55:45 +0000

    revert stripe checkout fix

--- packages/stripe_checkout/lib/stripe_checkout.dart
+++ packages/stripe_checkout/lib/stripe_checkout.dart
@@ -1,9 +1,10 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:stripe_checkout/src/platforms/checkout.dart'
     show CheckoutResponse;
 
-import 'src/platforms/stripe_checkout_web.dart' as stripe;
+import 'src/platforms/checkout.dart';
+import 'src/platforms/stripe_checkout.dart'
+    if (dart.library.js) 'src/platforms/stripe_checkout_web.dart' as stripe;
 
 Future<CheckoutResponse> redirectToCheckout({
   required BuildContext context,
@@ -13,17 +14,12 @@ Future<CheckoutResponse> redirectToCheckout({
   String? successUrl,
   String? canceledUrl,
 }) {
-  if (kIsWeb) {
-    return stripe.redirectToCheckout(
-      context: context,
-      sessionId: sessionId,
-      publishableKey: publishableKey,
-      stripeAccountId: stripeAccountId,
-      successUrl: successUrl,
-      canceledUrl: canceledUrl,
-    );
-  } else {
-    throw UnsupportedError(
-        'redirectToCheckout is not supported on this platform');
-  }
+  return stripe.redirectToCheckout(
+    context: context,
+    sessionId: sessionId,
+    publishableKey: publishableKey,
+    stripeAccountId: stripeAccountId,
+    successUrl: successUrl,
+    canceledUrl: canceledUrl,
+  );
 }
