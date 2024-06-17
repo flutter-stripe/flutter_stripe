9f798790060690d7643c6651f1496a9350200bd2
Remon <remonhelmond+dev@gmail.com>
2024-06-17 17:59:07 +0100

    fix #1753 make sure that it is clear that enablepostal code only works for android

--- packages/stripe/lib/src/widgets/card_form_field.dart
+++ packages/stripe/lib/src/widgets/card_form_field.dart
@@ -49,6 +49,8 @@ class CardFormField extends StatefulWidget {
   /// Defaults is `true`. If your configuration in Stripe requires a postalcode
   /// check as defined in https://stripe.com/docs/radar/rules#traditional-bank-checks
   /// make sure this one is set to `true`.
+  ///
+  /// This is only supported on android
   final bool enablePostalCode;
 
   /// Android only: Controls the postal code entry shown (when `enablePostalCode` is set to true).
@@ -217,7 +219,7 @@ class _MethodChannelCardFormField extends StatefulWidget {
     Key? key,
     this.onFocus,
     this.style,
-    this.enablePostalCode = false,
+    this.enablePostalCode = true,
     double? width,
     double? height,
     BoxConstraints? constraints,
