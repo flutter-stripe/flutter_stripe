7f557b16118da073370c62e74ba7d61f6e2a3399
Giacomo Policicchio <giacomo.Policicchio@tui.com>
2024-07-17 20:36:06 +0200

    Rollback Change on Stripe Ios Sdk

--- packages/stripe_ios/ios/Classes/Stripe Sdk/StripeSdk+PaymentSheet.swift
+++ packages/stripe_ios/ios/Classes/Stripe Sdk/StripeSdk+PaymentSheet.swift
@@ -224,11 +224,9 @@ extension StripeSdk {
             mode = PaymentSheet.IntentConfiguration.Mode.payment(
                 amount: amount,
                 currency: modeParams["currencyCode"] as? String ?? "",
-                setupFutureUsage: { return switch modeParams["setupFutureUsage"] as? String {
-                                                      case "OffSession":  .offSession
-                                                      case "OnSession":  .onSession
-                                                      default:  nil
-                                                  } }(),
+                setupFutureUsage: modeParams["setupFutureUsage"] != nil
+                    ? (modeParams["setupFutureUsage"] as? String == "OffSession" ? .offSession : .onSession)
+                    : nil,
             captureMethod: captureMethod
             )
         } else {
