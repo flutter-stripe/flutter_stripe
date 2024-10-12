47f9ce1e0cb9994e6738fecb7c9b93d65d1e5295
cedvandenbosch <cedvandenbosch@tyxio.com>
2024-10-12 23:28:20 +0200

    add confirm setup

--- packages/stripe_js/lib/src/js/setup_intents/confirm_setup.dart
+++ packages/stripe_js/lib/src/js/setup_intents/confirm_setup.dart
@@ -1,25 +1,31 @@
-import 'package:stripe_js/stripe_api.dart';
-import 'package:stripe_js/stripe_js.dart';
-import '../utils/utils.dart';
+import 'dart:async';
 import 'dart:js_interop';
 
-extension ExtensionConfirmCardSetup on Stripe {
-  Future<SetupIntentResponse> confirmSetup(
-    String clientSecret, {
-    ConfirmCardSetupData? data,
-    ConfirmCardSetupOptions? options,
-  }) {
-    final jsData = (data?.toJson() ?? {}).jsify();
-    final jsOptions = (options?.toJson() ?? {}).jsify();
-    return _confirmCardSetup(clientSecret, jsData, jsOptions)
+import 'package:stripe_js/src/api/setup_intents/confirm_setup_options.dart';
+import 'package:stripe_js/stripe_js.dart';
+
+extension ExtensionConfirmPayment on Stripe {
+  /// Use stripe.confirmSetup to confirm a SetupIntent using data collected
+  /// by the Payment Element, or with manually provided data via confirmParams.
+  /// When called, stripe.confirmSetup will attempt to complete any required actions,
+  /// such as authenticating your user by displaying a 3DS dialog or redirecting
+  /// them to a bank authorization page. Your user will be redirected to the
+  /// return_url you pass once the authorization is complete.
+  ///
+  /// [confirmSetup] will return a Future. Upon a successful authorization,
+  /// your user will be redirected to the return_url you provide
+  /// before the Future ever resolves.
+  /// see: https://docs.stripe.com/js/setup_intents/confirm_setup
+  Future<void> confirmSetup(
+    ConfirmSetupOptions options,
+  ) async {
+    final completer = Completer();
+    _confirmSetup(options.toJson().jsify())
         .toDart
-        .then((response) => response.toDart);
+        .then((response) => completer.complete());
+    return completer.future;
   }
 
   @JS('confirmSetup')
-  external JSPromise<JSSetupIntentResponse> _confirmCardSetup(
-    String clientSecret, [
-    JSAny? data,
-    JSAny? options,
-  ]);
+  external JSPromise<JSAny> _confirmSetup(JSAny? options);
 }
