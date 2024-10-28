d73d6336cc0966a5389c002b025d43c5d8007cac
cedvdb <cedvandenbosch@gmail.com>
2024-10-28 21:10:45 +0100

    fix example (#1974)
    Co-authored-by: cedvandenbosch <cedvandenbosch@tyxio.com>
    Co-authored-by: Rémon <remonhelmond@gmail.com>

--- example/lib/screens/payment_sheet/express_checkout/platforms/express_checkout_element.dart
+++ example/lib/screens/payment_sheet/express_checkout/platforms/express_checkout_element.dart
@@ -1,13 +1,7 @@
 import 'package:flutter/widgets.dart';
-import 'package:flutter_stripe_web/flutter_stripe_web.dart';
-import 'package:stripe_example/screens/payment_sheet/express_checkout/platforms/express_checkout_element_web.dart';
 
 Future<void> pay() async {
-  await WebStripe.instance.confirmPaymentElement(
-    ConfirmPaymentElementOptions(
-      confirmParams: ConfirmPaymentParams(return_url: getReturnUrl()),
-    ),
-  );
+  throw UnimplementedError();
 }
 
 class ExpressCheckoutWidget extends StatelessWidget {
