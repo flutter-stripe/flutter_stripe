0221611cabecdde7b502fcd30fd021a9629ee444
Eduard Dumitrescu <eduard.dumitrescu.ext@simplydelivery.de>
2024-11-20 15:31:35 +0200

    add default value to payment method

--- packages/stripe_js/lib/src/api/payment_methods/payment_method.dart
+++ packages/stripe_js/lib/src/api/payment_methods/payment_method.dart
@@ -78,6 +78,7 @@ class PaymentMethod with _$PaymentMethod {
     /// An additional hash is included on the PaymentMethod with a name
     /// matching this value.
     /// It contains additional information specific to the PaymentMethod type.
+    @JsonKey(unknownEnumValue: PaymentMethodType.unknown)
     required PaymentMethodType type,
   }) = _PaymentMethod;
 
