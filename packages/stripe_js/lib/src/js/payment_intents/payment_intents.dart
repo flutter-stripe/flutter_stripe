dd700b5c570f5d1abe0879416d4b187345aa8db1
Amadeusz Blanik <amadeusz@blanik.me>
2024-12-06 08:22:44 +0100

    Add P24 confirm payment functionality for web integration

--- packages/stripe_js/lib/src/js/payment_intents/payment_intents.dart
+++ packages/stripe_js/lib/src/js/payment_intents/payment_intents.dart
@@ -2,6 +2,7 @@ export 'confirm_acss_debit_payment.dart';
 export 'confirm_alipay_payment.dart';
 export 'confirm_card_payment.dart';
 export 'confirm_ideal_payment.dart';
+export 'confirm_p24_payment.dart';
 export 'confirm_payment.dart';
 export 'confirm_sepa_debit_payment.dart';
 export 'handle_card_action.dart';
