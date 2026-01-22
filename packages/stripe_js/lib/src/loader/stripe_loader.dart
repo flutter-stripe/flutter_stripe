4e2076c77db6dfbd8e30f4d2e66d070d7cd5c475
Meylis Annagurbanov <meylis.annagurbanow98@gmail.com>
2026-01-22 19:06:04 +0500

    fix(stripe_js): simplify script loading and fix onload callback
    The previous implementation was timing out because the onload callback
    wasn't being invoked correctly. Simplified to directly complete the
    completer on script load, without intermediate callback mechanism.
    Added onerror handler for better error reporting.

--- packages/stripe_js/lib/src/loader/stripe_loader.dart
+++ packages/stripe_js/lib/src/loader/stripe_loader.dart
@@ -8,29 +8,22 @@ import 'dart:js_interop_unsafe';
 Future<void> _injectSrcScript(String src, String windowVar) async {
   Completer completer = Completer();
 
-  // Create callback function that will be called when script loads
-  void onLoadCallback(JSAny? _) {
-    completer.complete();
-  }
-
-  globalContext['ff_trigger_$windowVar'] = (onLoadCallback).toJS;
-
   // Create and inject the script tag
   HTMLScriptElement script = HTMLScriptElement();
   script.type = 'text/javascript';
   script.src = src;
   script.crossOrigin = 'anonymous';
   script.onload = ((JSAny? _) {
-    if (globalContext[windowVar] != null) {
-      globalContext.callMethod('ff_trigger_$windowVar'.toJS);
-    }
+    completer.complete();
+  }).toJS;
+  script.onerror = ((JSAny? _) {
+    completer.completeError(Exception('Failed to load $src'));
   }).toJS;
 
   assert(document.head != null);
   document.head!.append(script);
 
   await completer.future;
-  globalContext.delete('ff_trigger_$windowVar'.toJS);
 }
 
 // Initializes the Stripe JS SDKs by injecting them into the `head` of the
