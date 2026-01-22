6238ac4c7247f1f6287b2c20d31dec220309f3ac
Remon <remonhelmond+dev@gmail.com>
2026-01-22 13:26:02 +0000

    Revert "Feat: web language (#2267)"
    This reverts commit 903f367c2a4ece383694d536a583cefc9206ac7a.

--- packages/stripe_platform_interface/lib/src/stripe_platform_interface.dart
+++ packages/stripe_platform_interface/lib/src/stripe_platform_interface.dart
@@ -32,7 +32,6 @@ abstract class StripePlatform extends PlatformInterface {
     String? merchantIdentifier,
     String? urlScheme,
     bool? setReturnUrlSchemeOnAndroid,
-    String? locale,
   });
 
   Future<PaymentMethod> createPaymentMethod(
