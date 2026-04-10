f0b54e3e609e9843bab8a968bb66a7dfe248d189
Remon <remonhelmond+dev@gmail.com>
2026-04-10 13:09:12 +0100

    fix: improve pubspec items to comply to pub.dev

--- packages/stripe_js/lib/src/js/utils/parse_intent_response.dart
+++ packages/stripe_js/lib/src/js/utils/parse_intent_response.dart
@@ -1,4 +1,3 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:stripe_js/stripe_api.dart';
 import 'dart:js_interop';
 
@@ -10,28 +9,24 @@ extension type JSMap._(JSAny o) implements JSAny {
 }
 
 extension type JSIntentResponse._(JSMap o) implements JSMap {
-  @redeclare
   PaymentIntentResponse get toDart {
     return PaymentIntentResponse.fromJson(o.toDart);
   }
 }
 
 extension type JSSetupIntentResponse._(JSMap o) implements JSMap {
-  @redeclare
   SetupIntentResponse get toDart {
     return SetupIntentResponse.fromJson(o.toDart);
   }
 }
 
 extension type JSPaymentMethodResponse._(JSMap o) implements JSMap {
-  @redeclare
   PaymentMethodResponse get toDart {
     return PaymentMethodResponse.fromJson(o.toDart);
   }
 }
 
 extension type JSTokenResponse._(JSMap o) implements JSMap {
-  @redeclare
   TokenResponse get toDart {
     return TokenResponse.fromJson(o.toDart);
   }
