795f882b98ec12e10ae3e79785756aaa838df434
Jonas Bark <jonas.bark@gmx.de>
2026-05-13 10:28:32 +0200

    Sync iOS with React Native v0.65.1

--- packages/stripe_ios/ios/stripe_ios/Sources/stripe_ios/Stripe Sdk/AddressSheet/AddressSheetView.swift
+++ packages/stripe_ios/ios/stripe_ios/Sources/stripe_ios/Stripe Sdk/AddressSheet/AddressSheetView.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 import StripePaymentSheet
 
 @objc(AddressSheetView)
--- packages/stripe_ios/ios/stripe_ios/Sources/stripe_objc/Stripe Sdk/StripeSdkEventEmitterCompat.h
+++ packages/stripe_ios/ios/stripe_ios/Sources/stripe_objc/Stripe Sdk/StripeSdkEventEmitterCompat.h
@@ -1,9 +1,9 @@
 // This is a compat layer for NativeStripeSdkModuleSpecBase which is generated with codegen
 // for the new arch. This implements the same methods, but using old arch apis.
 
-#import <React/RCTEventEmitter.h>
+#import <Foundation/Foundation.h>
 
-@interface StripeSdkEventEmitterCompat : RCTEventEmitter
+@interface StripeSdkEventEmitterCompat : NSObject
 - (void)emitOnConfirmHandlerCallback:(NSDictionary *)value;
 - (void)emitOnConfirmationTokenHandlerCallback:(NSDictionary *)value;
 - (void)emitOnFinancialConnectionsEvent:(NSDictionary *)value;
@@ -26,5 +26,7 @@
 - (void)emitOnCheckoutClientSecretRequested:(NSDictionary *)value;
 - (void)emitPaymentMethodMessagingElementDidUpdateHeight:(NSDictionary *)value;
 - (void)emitPaymentMethodMessagingElementConfigureResult:(NSDictionary *)value;
+
+- (void)sendEventWithName:(NSString *)name body:(NSDictionary<NSString *, id> *)body;
 @end
 
